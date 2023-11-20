479abf965d421229f7bc4c2990bb62334f9ba647
m-shehzad455 <shehzuali5@gmail.com>
2023-11-20 18:55:51 +0500

    update files

--- .next/static/webpack/webpack.cbc03627eaf6ed2c.hot-update.js
+++ .next/static/webpack/webpack.cbc03627eaf6ed2c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "eee4744233897ab4"; }
+/******/ 	__webpack_require__.h = function() { return "1e12a8239fb58313"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- src/components/blocks/navbar/partials/Social.tsx
+++ src/components/blocks/navbar/partials/Social.tsx
@@ -2,7 +2,7 @@ const Social = () => {
   return (
     <li className="nav-item">
       <nav className="nav social social-muted justify-content-end text-end">
-        <a href="https://www.linkedin.com/in/shehxad-hussayn-762030213/ ">
+        <a href="https://www.linkedin.com/in/shehzad-hussain-762030213/ ">
           <i className="uil uil-linkedin" />
         </a>
 
