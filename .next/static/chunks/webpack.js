b327f21bb9aad81ba536fd137ed838a12c88f418
m-shehzad455 <shehzuali5@gmail.com>
2023-11-27 20:57:25 +0500

    update footer section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7fe5c52fa12dfd4b"; }
+/******/ 		__webpack_require__.h = function() { return "ee1e8353366c356b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/reuseable/SocialLinks.tsx
+++ src/components/reuseable/SocialLinks.tsx
@@ -5,11 +5,11 @@ type SocialLinksProps = { className?: string };
 // ========================================================
 
 const links = [
-  { id: 1, icon: 'uil uil-twitter', url: 'https://twitter.com/uilibofficial' },
-  { id: 2, icon: 'uil uil-facebook-f', url: 'https://facebook.com/uiLibOfficial/' },
-  { id: 3, icon: 'uil uil-dribbble', url: '#' },
-  { id: 4, icon: 'uil uil-instagram', url: 'https://www.instagram.com/uilibofficial/' },
-  { id: 5, icon: 'uil uil-youtube', url: 'https://www.youtube.com/channel/UCsIyD-TSO1wQFz-n2Y4i3Rg' }
+  { id: 1, icon: 'uil uil-twitter', url: 'https://twitter.com/shehxadshehzu' },
+  { id: 2, icon: 'uil uil-github', url: 'https://github.com/shehxad-hussain' },
+  { id: 3, icon: 'uil uil-linkedin', url: 'https://www.linkedin.com/in/shehzad-hussain-762030213/ ' },
+  { id: 4, icon: 'uil uil-instagram', url: 'https://www.instagram.com/shehzunorthman/' },
+  { id: 5, icon: 'uil uil-youtube', url: 'https://www.youtube.com/channel/UCR9z7M8rRsPMd65Q2ZyTHww' }
 ];
 
 const SocialLinks: FC<SocialLinksProps> = ({ className = 'nav social social-white mt-4' }) => {
